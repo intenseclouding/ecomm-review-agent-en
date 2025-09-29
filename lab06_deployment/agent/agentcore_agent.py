6bf74ea177250e923fd1bbd55c1ac01d9ff17935
swkye <swkye@amazon.com>
2025-09-29 17:36:23 +0900

    lab06 code add

---
+++ lab06_deployment/agent/agentcore_agent.py
@@ -0,0 +1,12 @@
+from bedrock_agentcore import BedrockAgentCoreApp
+from agent.orchestrator.agent import comprehensive_analyzer
+
+app = BedrockAgentCoreApp()
+
+@app.entrypoint()
+def invoke_agent(payload):
+    response = comprehensive_analyzer(payload)
+    return {"result": response}
+
+if __name__ == "__main__":
+    app.run()

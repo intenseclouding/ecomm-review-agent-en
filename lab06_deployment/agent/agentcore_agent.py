40df7b77ca41035e94ab10fa86e041a12d0c3e89
swkye <swkye@amazon.com>
2025-10-10 10:28:58 +0900

    feat: code clean

--- lab06_deployment/agent/agentcore_agent.py
+++ lab06_deployment/agent/agentcore_agent.py
@@ -8,7 +8,9 @@ app = BedrockAgentCoreApp()
 def invoke_agent(payload):
     review_data = payload.get("review_data")
     product_data = payload.get("product_data")
-    response = comprehensive_analyzer(product_data = product_data, review_data = review_data)
+    response = comprehensive_analyzer(
+        product_data=product_data, review_data=review_data
+    )
     return {"result": response}
 
 
